9d63f115c5941f0687c4281461fd7d12cea9ac14
Vitor Lima <62509152+divirjo@users.noreply.github.com>
2022-12-24 02:53:15 +0000

    Configuração static files

--- BetterGlauco/settings.py
+++ BetterGlauco/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', os.environ.get('HOST_TESTES')]
+ALLOWED_HOSTS = ['.vercel.app', os.environ.get('HOST_TESTES')]
 
 
 # Application definition
@@ -134,9 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = "/static/"
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
