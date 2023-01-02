f4fff490f9170cbbfc9b6b515d6c6be9beac40c4
Vitor Lima <62509152+divirjo@users.noreply.github.com>
2023-01-02 23:27:24 +0000

    Formulário da página de pesquisa valor fundo

--- investimento/urls.py
+++ investimento/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Ajuda, HomeInvestimento
+from .views import Ajuda, HomeInvestimento, Valor_compra
 
 
 app_name='investimento'
 
 urlpatterns = [
-    path('help/', Ajuda.as_view(), name='ajuda_inicio'),
     path('', HomeInvestimento.as_view(), name='home_investimento'),
+    path('help/', Ajuda.as_view(), name='ajuda_inicio'),
+    path('valor_compra/', Valor_compra.as_view(), name='valor_compra'),
     ]
--- investimento/views.py
+++ investimento/views.py
@@ -1,11 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import FormView, TemplateView
+
 
 class HomeInvestimento(LoginRequiredMixin, TemplateView):
     template_name = 'invest_inicio.html'
-    
+
     def get_login_url(self) -> str:
         return super().get_login_url()
 
+
 class Ajuda(LoginRequiredMixin, TemplateView):
     template_name = 'ajuda_inicio.html'
+    
+    
+class Valor_compra(LoginRequiredMixin, TemplateView):
+    template_name = 'valor_compra.html'
+    
+    def get_queryset(self):
+        ticket = self.request.GET.get('ticket')
+        return None
