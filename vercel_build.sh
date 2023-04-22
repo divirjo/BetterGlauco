dc0a53adbd8a890dca3b6cbdb79f5fa1ddd31297
divirjo <worldoftyto@gmail.com>
2023-04-22 15:33:03 -0300

    versel echo python version

--- vercel_build.sh
+++ vercel_build.sh
@@ -5,6 +5,7 @@ echo "### Instalar dependências ###"
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python -V
+python3.9 -V
 
 echo "### Executando as migrações ###"
 python3.9 manage.py makemigrations --noinput
