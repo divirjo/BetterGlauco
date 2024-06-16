5b94ad969af1265de969050d5be6839c39e01074
divirjo <worldoftyto@gmail.com>
2024-06-16 16:52:25 -0300

    atualização versão python

--- vercel_build_files.sh
+++ vercel_build_files.sh
@@ -5,11 +5,11 @@ echo "### Instalar dependências ###"
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python -V
-python3.9 -V
+python3.12 -V
 
 echo "### Executando as migrações ###"
-python3.9 manage.py makemigrations 
-python3.9 manage.py migrate 
+python3.12 manage.py makemigrations 
+python3.12 manage.py migrate 
 
 echo "### Obter arquivos estáticos ###"
-python3.9 manage.py collectstatic 
+python3.12 manage.py collectstatic 
